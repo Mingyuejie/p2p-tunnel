a9b213322609ad2f8e491bdb94cd3092b1c86c7c
韦伟平 <iswwping@163.com>
2021-11-05 11:35:42 +0800

    同步

--- client.web.vue3/src/apis/request.js
+++ client.web.vue3/src/apis/request.js
@@ -2,7 +2,7 @@
  * @Author: snltty
  * @Date: 2021-08-19 23:04:50
  * @LastEditors: snltty
- * @LastEditTime: 2021-10-26 12:58:04
+ * @LastEditTime: 2021-11-05 11:09:40
  * @version: v1.0.0
  * @Descripttion: 功能说明
  * @FilePath: \client.web.vue3\src\apis\request.js
@@ -92,7 +92,7 @@ const onWebsocketMsg = (msg) => {
     }
 }
 const initWebsocket = () => {
-    ws = new WebSocket('ws://127.0.0.1:59410');
+    ws = new WebSocket('ws://127.0.0.1:59415');
     ws.onopen = onWebsocketOpen;
     ws.onclose = onWebsocketClose
     ws.onmessage = onWebsocketMsg
