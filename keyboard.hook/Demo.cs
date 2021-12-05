d0ac2a8719d61ead9280d65f2e765b2cdf30b779
wwping <iswwping@163.com>
2021-12-05 10:57:18 +0800

    键盘钩子学习

---
+++ keyboard.hook/Demo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keyboard.hook
+{
+    internal class Demo
+    {
+        KeyboardHook hook = new KeyboardHook();
+
+        public void Lock()
+        {
+            hook.Start();
+
+            //控制台 消息泵
+            //Hook.tagMSG msg = new Hook.tagMSG();
+            //while (Hook.GetMessage(ref msg, 0, 0, 0) > 0)
+            //{
+            //    if (!isStart)
+            //    {
+            //        break;
+            //    }
+            //}
+        }
+
+        public void UnLock()
+        {
+            hook.Dispose();
+        }
+    }
+}
